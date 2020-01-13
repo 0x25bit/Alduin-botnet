759d473ca99e8aa1c290153b685851ee719e6442
Mogyiii <szabo.mate960722@gmail.com>
2020-01-13 14:55:57 +0100

    Create List and fixing Layout

---
+++ Alduin/Alduin.Web/Controllers/ListController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alduin.Web.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
